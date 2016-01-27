4becc171f13b480a6833eb7c0d2bdc57a887fb5c
David Lozic <david.lozic@fer.hr>
2016-01-27 11:40:06 +0100

    something

---
+++ src/Assignments.hs
@@ -0,0 +1,33 @@
+module Assignments where
+
+import Data.Time
+import System.Directory
+
+-- | Academic year shorthand (e.g. 2015 for 2015/16)
+type Year = Integer
+
+-- | An assignment type
+data AType = Homework | Exam | Project deriving (Show, Read)
+
+-- | A an assignment configuration data structure
+-- | Uses Data.Time.UTCTime
+-- | If files is an empty list, ANY number of files are OK
+data Configuration = Configuration { 
+	published 	 :: UTCTime, -- When to publish
+	deadline 	 :: UTCTime, -- Submission deadline
+	lateDeadline :: UTCTime, -- Late submission deadline
+	files 		 :: [String], -- File names to expect
+	minScore 	 :: Double, -- Minimum achievable
+	maxScore 	 :: Double,	-- Maximum achievable
+	required 	 :: Double	-- Score req to pass
+-- Anything else you might think would be useful
+} deriving (Show, Read)
+
+-- | An assignment descriptor
+data Assignment = Assignment { 
+	year :: Year, 
+	aType :: AType,
+	number :: Int
+-- Anything else you might think would be useful
+} deriving (Show, Read)
+
